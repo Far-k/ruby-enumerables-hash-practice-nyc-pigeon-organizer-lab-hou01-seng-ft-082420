d8eea395921458c34c9c9a2cdd2838ebd94378ca
Farzana Karim <farzana.mkarim@gmail.com>
2020-08-01 18:35:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[names]
+          final_array[names] = {}
+end
+if !final_array[name][key]
+   !final_array[name][key] = []
+end
+final_array[name][key].push(inner_key)
+end
+end
+end
 end
